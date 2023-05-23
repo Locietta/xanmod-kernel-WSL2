23b5f47e5be7609382752276ea8f9899b1e96e87
Locietta <locietta@gmail.com>
2023-05-23 12:39:50 +0800

    Revert "A possible solution to resolve release conflict."
    This reverts commit c6c7bd67f9e1d9b69eefddad9a7ad6ba319599fd.

--- config-lts.sh
+++ config-lts.sh
@@ -8,7 +8,6 @@ git apply ../0002-dxgkrnl-enable-lts-support.patch
 # generate .config
 #
 cp ../wsl2_defconfig ./arch/x86/configs/wsl2_defconfig
-sed -i 's/CONFIG_LOCALVERSION=".*"/CONFIG_LOCALVERSION="-locietta-WSL2-lts"/' ./arch/x86/configs/wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 wsl2_defconfig
 # scripts/config -e MSKYLAKE
 # scripts/config -e PERF_EVENTS_INTEL_UNCORE
