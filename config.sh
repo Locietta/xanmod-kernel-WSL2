c187df0d2f805f75d354018359a1910e4a451d50
Locietta <locietta@qq.com>
2023-08-18 18:24:46 +0800

    [Patch] Switch to LLVM-git and Fix Clang Build (#40)
    * fix: remove flags not available in clang 15
    
    * ci: move toolchain to llvm-git, drop patch for clang15 workaround
    
    * ci: use wildcard for safe directory workaround
    
    * ci: install llvm and upgrade all packages
    
    LLVM is already trimmed from upstream container
    
    * patch: fix LTO build with SRSO
    
    patch to fix this is from https://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git/diff/?id=79cd2a11224eab86d6673fe8a11d2046ae9d2757
    
    Ref: https://github.com/ClangBuiltLinux/linux/issues/1909
    
    * patch: adapt srso lto fix to 6.1
    
    * misc: fix typo, rename `build-lts` to `build`

--- config.sh
+++ config.sh
@@ -3,12 +3,14 @@
 #
 git apply ../0001-6.1.y-dxgkrnl.patch
 git apply ../0002-dxgkrnl-enable-mainline-support.patch
-git apply ../0003-revert-xanmod-gcc-options.patch
-
+git apply ../0003-srso-lto.patch
+if [ $? != 0 ]; then
+    echo "Patch conflict!"
+    exit 1 # so it can be catched by github action
+fi
 #
 # generate .config
 #
 cp ../wsl2_defconfig ./arch/x86/configs/wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 wsl2_defconfig
-# scripts/config -e MSKYLAKE
-# scripts/config -e PERF_EVENTS_INTEL_UNCORE
+
