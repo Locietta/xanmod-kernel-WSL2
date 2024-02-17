ebd41eec35b65c767999554155d83b6b891c7785
Locietta <locietta@qq.com>
2024-02-17 11:45:18 +0800

    [Kernel Patch] Migrate Patches for 6.6 LTS (#49)
    patch: migrate patches for 6.6 LTS

--- config-lts.sh
+++ config-lts.sh
@@ -2,7 +2,9 @@
 # Apply dxgkrnl patch
 #
 git apply ../0001-6.1.y-dxgkrnl.patch
-git apply ../0002-dxgkrnl-enable-lts-support.patch
+git apply ../0002-dxgkrnl-enable-mainline-support.patch
+git apply ../0003-bbrv3-fix-clang-build.patch
+git apply ../0004-nf_nat_fullcone-fix-clang-uninitialized-var-werror.patch
 
 if [ $? != 0 ]; then
     echo "Patch conflict!"
