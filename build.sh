35ef71f30bf8d9be5e883f392f1e8d7c971ff117
taalojarvi <sreedevan05@gmail.com>
2024-06-02 13:07:01 +0530

    build.sh: Use logical cores for parallel build based on SMT
    This commit modifies the build script to leverage the system's available logical cores for parallel compilation.
    
    Changes:
    - Added code to detect Hyper-Threading (SMT) status using /sys/devices/system/cpu/smt/active.
    - Introduced the LOGICAL_CORES environment variable to store the number of logical cores (physical cores multiplied by 2 if SMT is enabled).
    - Updated the make command to use $LOGICAL_CORES for the -j option, ensuring efficient parallel build execution.
    
    This change improves build performance by utilizing the full potential of the CPU hardware, especially on systems with Hyper-Threading enabled.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #
+if [ "$(cat /sys/devices/system/cpu/smt/active)" = "1" ]; then
+	export LOGICAL_CORES=$(($(nproc --all) * 2))
+else
+	export LOGICAL_CORES=$(nproc --all)
+fi
 # Compile
 #
-make LLVM=1 LLVM_IAS=1 -j$(nproc)
+make LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
