85c518d18b83deddd5e3d79b2c57c55e13b51182
taalojarvi <sreedevan05@gmail.com>
2024-06-04 09:47:12 +0530

    build.sh: Print out number of cores to build log
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ fi
 # Compile
 #
 if [ "$IS_LTS" = "NO" ]; then
+	echo -e "Using $LOGICAL_CORES jobs for this non-LTS build..."
 	make CC='ccache clang -Qunused-arguments -fcolor-diagnostics' LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
 else
+	echo -e "Using $LOGICAL_CORES jobs for this LTS build..."
 	make LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
 fi
