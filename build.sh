4298ecf9f2db14a2aa05aec8e3c05b094e4d7a12
Karthik Sreedevan <sreedevan05@gmail.com>
2024-06-20 11:56:49 +0530

    RFC: I Shaved 63% Off My Build Time with This One Weird Trick (Developers HATE this!) (#66)
    * build.sh: Use logical cores for parallel build based on SMT
    
    This commit modifies the build script to leverage the system's available logical cores for parallel compilation.
    
    Changes:
    - Added code to detect Hyper-Threading (SMT) status using /sys/devices/system/cpu/smt/active.
    - Introduced the LOGICAL_CORES environment variable to store the number of logical cores (physical cores multiplied by 2 if SMT is enabled).
    - Updated the make command to use $LOGICAL_CORES for the -j option, ensuring efficient parallel build execution.
    
    This change improves build performance by utilizing the full potential of the CPU hardware, especially on systems with Hyper-Threading enabled.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    * CI: Add support for ccache
    
    As a failsafe, do not use CCACHE if its an LTS build.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    * workflows: Remove additional -y flag from pacman
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    * workflows: Build LTS branch on a monthly schedule.
    
    This will help reduce time spent on actions
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    * CI: Increase ccache 'max-size' to 2048M
    
    It's set to 500M by default which does not yield enough ccache hits
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    * CI: Cache each arch seperately
    
    Since we target different archs, it makes more sense to cache them seperately.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    * build.sh: Print out number of cores to build log
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    
    ---------
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #
+if [ "$(cat /sys/devices/system/cpu/smt/active)" = "1" ]; then
+	export LOGICAL_CORES=$(($(nproc --all) * 2))
+else
+	export LOGICAL_CORES=$(nproc --all)
+fi
 # Compile
 #
-make LLVM=1 LLVM_IAS=1 -j$(nproc)
+if [ "$IS_LTS" = "NO" ]; then
+	echo -e "Using $LOGICAL_CORES jobs for this non-LTS build..."
+	make CC='ccache clang -Qunused-arguments -fcolor-diagnostics' LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
+else
+	echo -e "Using $LOGICAL_CORES jobs for this LTS build..."
+	make LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
+fi
