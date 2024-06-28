ca3e39e8eb8e5368c3911b958ec7fa6425e54835
taalojarvi <sreedevan05@gmail.com>
2024-06-29 02:48:35 +0530

    config.sh: Unify config.sh scripts across LTS and non-LTS builds.
    Comparing the two config scripts, the only difference is that for LTS builds, a patch is omitted.
    
    Assuming the patch does not apply, detect if the build targets LTS branch (through ENV we created in a previous PR, and gracefully proceed to build if true.
    
    While we are at it, remove config-lts.sh and all mentions of it.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- config.sh
+++ config.sh
@@ -9,12 +9,16 @@ for patch_file in "$PATCH_DIR/"*.patch; do
 
   # Check for patch conflict (exit code 1)
   if [ $? -eq 1 ]; then
-    echo "Patch conflict encountered: $patch_file"
-    exit 1
+    echo -e "Patch conflict encountered: $patch_file"
+    if [ "$IS_LTS" = "NO" ]; then
+    	exit 1
+    else
+    	echo -e "git patch encountered errors, but choosing to continue as we are an LTS build"
+    fi  
   fi
 done
 
-echo -e "All patches applied successfully!"  
+
 
 cp ../wsl2_defconfig ./arch/x86/configs/wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 wsl2_defconfig
