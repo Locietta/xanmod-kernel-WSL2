004427fefe9f0a5d045da211a0eea35e7a579bb6
taalojarvi <sreedevan05@gmail.com>
2024-06-29 01:10:37 +0530

    config.sh: Move patches to its own subdirectory and recursively apply them
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- config.sh
+++ config.sh
@@ -1,21 +1,21 @@
-#
-# Apply dxgkrnl patch
-#
-git apply ../0001-6.1.y-dxgkrnl.patch
-git apply ../0002-dxgkrnl-enable-mainline-support.patch
-git apply ../0003-bbrv3-fix-clang-build.patch
-git apply ../0004-nf_nat_fullcone-fix-clang-uninitialized-var-werror.patch
-git apply ../0005-drivers-hv-dxgkrnl-Remove-argument-in-eventfd_signal.patch
+#!/bin/bash
 
-if [ $? != 0 ]; then
-    echo "Patch conflict!"
-    exit 1 # so it can be catched by github action
-fi
+PATCH_DIR="../patches"
 
-#
-# generate .config
-#
+# Loop through all patch files and apply them
+for patch_file in "$PATCH_DIR/"*.patch; do
+  echo -e "Applying patch: $patch_file"
+  git apply "$patch_file"
+
+  # Check for patch conflict (exit code 1)
+  if [ $? -eq 1 ]; then
+    echo "Patch conflict encountered: $patch_file"
+    exit 1
+  fi
+done
+
+echo -e "All patches applied successfully!"  
+
 cp ../wsl2_defconfig ./arch/x86/configs/wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 wsl2_defconfig
 make LLVM=1 LLVM_IAS=1 oldconfig
-
