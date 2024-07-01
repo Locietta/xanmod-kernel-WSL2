f9fc05c79e799072cdfb0d0cfc25d4b1b8ae9082
taalojarvi <sreedevan05@gmail.com>
2024-07-02 00:43:51 +0530

    config.sh: Enhance skip logic for non-LTS builds
    Introduces a check for the IS_LTS environment variable to conditionally apply patches based on build type.
    Patches containing "_skip_lts" will now be applied during non-LTS builds, even if the filename suggests skipping.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- config.sh
+++ config.sh
@@ -3,15 +3,24 @@
 PATCH_DIR="../patches"
 
 # Loop through all patch files and apply them
-# If the patch filename contains "skip_lts", patch will be ignored
+# If the patch filename contains "skip_lts", patch will be ignored.
+# The conditional will also check if the build is LTS before skipping and will peacefully ignore the skip tag if it is a non-LTS build.
 # TODO(taalojarvi): Expand conditional check to cover skipping patches according to buildtype.
 for patch_file in "$PATCH_DIR/"*.patch; do
   if [[ ! "$patch_file" =~ _skip_lts ]]; then
-  	echo -e "Applying patch: $patch_file"
+	echo -e "Applying patch: $patch_file"
   	git apply "$patch_file"
   else
-  	echo -e "Skipping patch: $patch_file"
+    	if [[ "$IS_LTS" = "NO" ]]; then
+  		echo -e "Patch skip ignored as this is a non LTS build."
+  		echo -e "Applying patch: $patch_file"
+  		git apply "$patch_file"
+  	else
+  		echo -e "Skipping patch: $patch_file"
+  	fi
+
   fi
+  
   # Check for patch conflict (exit code 1)
   if [ $? -eq 1 ]; then
     echo -e "Patch conflict encountered in $patch_file"
